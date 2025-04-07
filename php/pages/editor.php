cb7724935af569d9fe24662edbbac76bacfa2787
lasnelus <malabretienne@gmail.com>
2025-04-07 15:47:18 +0200

    creating the first part of a one-page system

---
+++ php/pages/editor.php
@@ -0,0 +1,15 @@
+<head>
+    <link rel="stylesheet" href="css/editor.css">
+</head>
+<div id="editorContainer">
+    <h1>Éditeur Markdown</h1>
+    <p>Éditez vos fichiers Markdown en toute simplicité.</p>
+    <p>Écrivez votre texte dans la zone de saisie ci-dessous et cliquez sur "Convertir" pour voir le résultat.</p>
+<div id="inputUser">
+    <input type="text" id="inputText" placeholder="Écrivez ici votre texte en Markdown..." />
+    <button id="convertButton">Convertir</button>
+</div>
+<div id="outputMarkdown">
+
+</div>
+</div>
---
+++ php/pages/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="content">
+        <h1>SuperMarkdownEditor</h1>
+        <p>Un éditeur Markdown simple et efficace.</p>
+        <p>Éditez vos fichiers Markdown en toute simplicité.</p>
+        <a href="./editor" class="button">Commencer MAINTENANT</a>
+    </div>
+</div>
