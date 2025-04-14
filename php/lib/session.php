2a3c97a180074c21b83aea685fa915f4330142f0
lasnelus <malabretienne@gmail.com>
2025-04-14 16:31:42 +0200

    login fonctionnel

--- php/lib/session.php
+++ php/lib/session.php
@@ -1,15 +1,4 @@
 <?php
-
-function loginSource($login) {
-    $res = false;
-    for($i=0;$i<strlen($login);$i++){
-        if($login[$i]=="@"){
-            $res = true;
-        }
-    }
-    return $res;
-}
-
 function connect(mysqli $conn, string $login, string $password): array {
     $hashPsw = hash("sha256", $password);
     echo "a";
@@ -36,20 +25,4 @@ function connect(mysqli $conn, string $login, string $password): array {
     print_r($res);
     return $res;
 }
-
-function addUser(mysqli $conn, string $login, $name, $firstName, $mail, string $password) {
-    if($login == "" || $password == "") { return false; }
-    $hashPsw = hash("sha256", $password);
-    $query = "INSERT INTO `user`(`pseudo`,`name`,`firstName`,`mail`,`pswd`) VALUES ('$login','$name' , '$firstName', '$mail', '$hashPsw')";
-    $ret = mysqli_query($conn, $query);
-    return $ret;
-}
-
-function deleteUser(mysqli $conn, int $id) {
-    if($id == 0) { return false; }
-    $query = "DELETE FROM `user` WHERE `id`='$id'";
-    $ret = mysqli_query($conn, $query);
-    return $ret;
-}
-
 ?>
