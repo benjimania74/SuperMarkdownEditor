b0c05655e9a41a11127bd0e1d8a16eb98f715d9f
lasnelus <malabretienne@gmail.com>
2025-04-25 16:55:00 +0200

    started a joke with aline and fixed a bit of team page

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (isset($_GET["action"])) {
     <script src="./js/front/front.js"></script>
 </head>
 
-<body>
+<body onload="add_message_bienvenue()">
     <?php
     include("html/header.html");
     ?>
