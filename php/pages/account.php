90ac882f5cde72e073d2281ca18465245e13fddc
lasnelus <malabretienne@gmail.com>
2025-04-25 12:58:42 +0200

    modif account fonctionnel ?

--- php/pages/account.php
+++ php/pages/account.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $newMail = $_POST["mail"];
 
     // Met à jour les informations utilisateur dans la base de données
-    $updateSuccess = updateUser($conn, $userId, $newPseudo, $newMail);
+    $updateSuccess = updateUser($conn, $userId, $newPseudo, $user["name"], $user["firstName"], $newMail, $user["pswd"]);
 
     if ($updateSuccess) {
         // Recharge les informations utilisateur après la mise à jour
@@ -19,63 +19,65 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 ?>
+
 <head>
     <link rel="stylesheet" href="css/account.css">
     <script src="js/front/account.js" defer></script>
 </head>
-< class="profileContainer">
-    <div class="profileHeader">
-        <div class="profileInfo">
-            <img src="./css/img/user_01.png" alt="Avatar" class="profileAvatar">
-            <div class="profileDetails">
-                <h1 id="profileName">
-                    <?php
-                    echo ($user["pseudo"])
-                        ?>
-                </h1>
-                <p id="profileEmail">
-                    <?php
-                    echo ($user["mail"])
-                        ?>
-                </p>
-                <button id="editProfileButton" class="button">Modifier le profil</button>
-                <a id="logOut" href="./logout" class="button">Déconnexion</a>
+<div class="profileHeader">
+    <div class="profileInfo">
+        <img src="./css/img/user_01.png" alt="Avatar" class="profileAvatar">
+        <div class="profileDetails">
+            <h1 id="profileName">
+                <?php
+                echo ($user["pseudo"])
+                    ?>
+            </h1>
+            <p id="profileEmail">
+                <?php
+                echo ($user["mail"])
+                    ?>
+            </p>
+            <button id="editProfileButton" class="button">Modifier le profil</button>
+            <a id="logOut" href="./logout" class="button">Déconnexion</a>
+        </div>
+        <!-- Formulaire de modification -->
+        <form id="editProfileForm" method="post" action="./account" style="display: none;">
+            <div class="input">
+                <label for="pseudo">Pseudo :</label>
+                <input type="text" id="pseudo" name="pseudo" value="<?php echo htmlspecialchars($user["pseudo"]); ?>"
+                    required>
             </div>
+            <div class="input">
+                <label for="mail">Email :</label>
+                <input type="email" id="mail" name="mail" value="<?php echo htmlspecialchars($user["mail"]); ?>"
+                    required>
+            </div>
+            <input type="submit" value="Enregistrer" class="button">
+            <button type="button" id="cancelEditButton" class="button">Annuler</button>
+        </form>
+    </div>
+</div>
+
+<!-- à changé pour du dom qui génère en fonction du nombre de projet -->
+<section class="projectsSection">
+    <h2>Mes Projets</h2>
+    <div class="projectsList">
+        <div class="projectCard">
+            <h3>Projet 1</h3>
+            <p>Dernière modification : 12/04/2025</p>
+            <a href="" class="button">Ouvrir</a>
+        </div>
+        <div class="projectCard">
+            <h3>Projet 2</h3>
+            <p>Dernière modification : 10/04/2025</p>
+            <a href="" class="button">Ouvrir</a>
+        </div>
+        <div class="projectCard">
+            <h3>Projet 3</h3>
+            <p>Dernière modification : 08/04/2025</p>
+            <a href="" class="button">Ouvrir</a>
         </div>
     </div>
-    <!-- Formulaire de modification -->
-    <form id="editProfileForm" method="post" action="./account.php" style="display: none;">
-        <div class="input">
-            <label for="pseudo">Pseudo :</label>
-            <input type="text" id="pseudo" name="pseudo" value="<?php echo htmlspecialchars($user["pseudo"]); ?>" required>
-        </div>
-        <div class="input">
-            <label for="mail">Email :</label>
-            <input type="email" id="mail" name="mail" value="<?php echo htmlspecialchars($user["mail"]); ?>" required>
-        </div>
-        <input type="submit" value="Enregistrer" class="button">
-        <button type="button" id="cancelEditButton" class="button">Annuler</button>
-    </form>
-
-    <!-- à changé pour du dom qui génère en fonction du nombre de projet -->
-    <section class="projectsSection">
-        <h2>Mes Projets</h2>
-        <div class="projectsList">
-            <div class="projectCard">
-                <h3>Projet 1</h3>
-                <p>Dernière modification : 12/04/2025</p>
-                <a href="" class="button">Ouvrir</a>
-            </div>
-            <div class="projectCard">
-                <h3>Projet 2</h3>
-                <p>Dernière modification : 10/04/2025</p>
-                <a href="" class="button">Ouvrir</a>
-            </div>
-            <div class="projectCard">
-                <h3>Projet 3</h3>
-                <p>Dernière modification : 08/04/2025</p>
-                <a href="" class="button">Ouvrir</a>
-            </div>
-        </div>
-    </section>
+</section>
 </div>
