8b7efa4b5c13052e4a220ee651a1ecea26255d32
Alem <mael.albrecht1@gmail.com>
2025-04-29 11:05:28 +0200

    update DOM project and create DOM folder

--- js/front/folder.js
+++ js/front/folder.js
@@ -1,12 +1,12 @@
-function folderView(folder){
+function fileView(file){
     const div = document.createElement("div");
     const h3 = document.createElement("h3");
     const a = document.createElement("a");
     const b = document.createElement("a");
 
     div.className = "projectCard";
-    div.id = "Dossier "+ folder["id"];
-    h3.innerHTML = folder["nameFolder"];
+    div.id = "Fichier "+ file["id"];
+    h3.innerHTML = file["nameFile"];
     div.appendChild(h3);
     a.className = "button";
     a.href = "";
@@ -19,22 +19,22 @@ function folderView(folder){
     return div;
 }
 
-function folderPoster(folders) {
+function filePoster(files) {
     const section = document.createElement("section");
     section.classList.add("projectsSection");
     const h2 = document.createElement("h2");
 
     section.className = "projectsSection";
-    h2.innerHTML = "Mes dossiers";
+    h2.innerHTML = "Mes fichiers";
     section.appendChild(h2);
 
-    var folderList = document.createElement("div");
+    var fileList = document.createElement("div");
 
-    for (let i in folders) {
-        folderList.appendChild(folderView(folders[i]));
+    for (let i in files) {
+        fileList.appendChild(fileView(files[i]));
     }
-    section.appendChild(folderList);
+    section.appendChild(fileList);
 
     var pc = document.querySelector(".profileContainer");
-    pc.appendChild(section);
+    pc.apppendChild(section);
 }
