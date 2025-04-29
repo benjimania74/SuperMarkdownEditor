38881b81d19fdd737fa900d3fee3990c6c47f66d
Benjamin B. <48529276+benjimania74@users.noreply.github.com>
2025-04-29 15:02:21 +0200

    Update domizer.js

--- js/mdInterpreter/domizer.js
+++ js/mdInterpreter/domizer.js
@@ -8,64 +8,22 @@ function toDOM(tokens) {
     domDiv.classList.add("md");
 
     tokens.forEach(token => {
-        var treated = token.treat(token.info);
-
+        var treated = token.treat(token.value);
         if(treated == null) {
-            treated = paragraphDomizer(token.info);
+            treated = paragraphDomizer(token.value);
         }
-
-        domDiv.appendChild( treated ); 
+        domDiv.appendChild( treated );
     });
     return domDiv;
 }
 
-function titleDomizer(info) {
-    var level = parseInt( info["level"] );
-    var value = info["value"];
-
-    var titleDiv = document.createElement("div");
-    titleDiv.classList.add("titleDiv");
-
-    var title = document.createElement("h" + level);
-    title.innerHTML = value;
-
-    titleDiv.appendChild(title);
-
-    if(level <= 3) {
-        titleDiv.appendChild( document.createElement("hr") );
-    }
-    return titleDiv;
+function paragraphDomizer(value) {
+    var p = document.createElement("p");
+    textTransformerToParent(p, value);
+    return p;
 }
 
-function horizontalLineDomizer(info) {
-    return document.createElement("hr");
-}
-
-function codeDomizer(info) {
-    var codeParser = getCodeParser(info["language"]);
-    var codePreTag = document.createElement("pre");
-    var value = info["value"];
-    
-    if(codeParser != undefined) {
-        codeParser( codePreTag, value );
-    } else {
-        codePreTag.innerHTML = value;
-    }
-    return codePreTag;
-}
-
-function lineReturnDomizer(info) {
-    return document.createElement("br");
-}
-
-function quoteDomizer(info) {
-    var quoteTag = document.createElement("p");
-    quoteTag.classList.add("quote");
-    quoteTag.innerHTML = info["value"];
-    return quoteTag;
-}
-
-function listDomizer(info) {
+/*function listDomizer(info) {
     var list = document.createElement("ul");
     var value = info["value"];
 
@@ -73,7 +31,8 @@ function listDomizer(info) {
         var line = value[i];
         if(typeof line == "string") {
             var element = document.createElement("li");
-            element.innerHTML = line;
+            textTransformerToParent(element, line);
+            //element.innerHTML = line;
             list.appendChild(element);
         } else {
             list.appendChild( line.treat( line.info ) );
@@ -90,17 +49,12 @@ function ordinatedListDomizer(info) {
         var line = value[i];
         if(typeof line == "string") {
             var element = document.createElement("li");
-            element.innerHTML = line;
+            textTransformerToParent(element, line);
+            //element.innerHTML = line;
             list.appendChild(element);
         } else {
             list.appendChild( line.treat( line.info ) );
         }
     }
     return list;
-}
-
-function paragraphDomizer(info) {
-    var p = document.createElement("p");
-    p.innerHTML = info["value"];
-    return p;
-}
+}*/
